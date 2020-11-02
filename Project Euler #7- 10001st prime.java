68f97b7180920c0ae46b240948912a3a11bfb73f
Nalin Nishant <36907019+nalin88@users.noreply.github.com>
2020-11-02 14:05:51 +0530

    Update Project Euler #7- 10001st prime.java

--- Project Euler #7- 10001st prime.java
+++ Project Euler #7- 10001st prime.java
@@ -7,35 +7,31 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
+        List<Long> list = new ArrayList<>();
+        list.add(2l);
+        for(long i = 3; i <= 1000000; i+=2){
+            boolean isprime = true;
+
+            for(long p : list){
+                if(p*p > i){
+                    break;
+                }
+                if(i%p == 0){
+                    isprime = false;
+                    break;
+                }
+            }
+            if(isprime){
+                list.add(i);
+            }
+        }
+
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        int[] prime = new int[10000];
-        prime[0]=2;
-        int k=1;
-        int i=3;
-        int count =1;
-        while(count<10000){
-             boolean flag = true;
-            for(int j=3;j<i/2;j++){
-                if(i%j==0){
-                    flag = false;
-                    break; 
-                }
-                
-                }
-            
-            if(flag==true){
-                prime[k]=i;
-                count++;
-                k++;
-                
-            }
-            i=i+2;
-           
-        } 
         for(int a0 = 0; a0 < t; a0++){
             int n = in.nextInt();
-            System.out.println(prime[n-1]);
+            System.out.println(list.get(n-1));
         }
+        in.close();
     }
 }
